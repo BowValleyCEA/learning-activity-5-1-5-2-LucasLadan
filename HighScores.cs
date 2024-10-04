a0f8722a596037a0634ccda71525d4bcf063d785
Lucas Ladan <l.ladan419@mybvc.ca>
2024-10-03 19:11:58 -0600

    Added the variables

--- HighScores.cs
+++ HighScores.cs
@@ -8,5 +8,13 @@ namespace game1401_la_5
 {
     internal class HighScores
     {
+        int _score;
+        string _name;
+
+        HighScores (int score, string name)
+        {
+            _score = score;
+            _name = name;
+        }
     }
 }
